c0e54c383244ae8a5b8b19a2c75ed25113bce6e3
alfi4000 <149228038+alfi4000@users.noreply.github.com>
2024-12-03 00:22:50 -0500

    Update media-exporter.php

--- media-exporter.php
+++ media-exporter.php
@@ -4,9 +4,9 @@ Plugin Name: Media Exporter
 Description: Exports media files information to a JSON file.
 Version: 1.0.0
 Author: alfi4000
-Author URL: https://github.com/alfi4000
-Plugin URL: https://github.com/alfi4000/simple-wordpress-plugins/blob/main/media-exporter.php
-Plugin Home Page URL: https://github.com/alfi4000/simple-wordpress-plugins
+Author URI: https://github.com/alfi4000
+Plugin URI: https://github.com/alfi4000/simple-wordpress-plugins/blob/main/media-exporter.php
+Plugin Home Page URI: https://github.com/alfi4000/simple-wordpress-plugins
 */
 
 // Exit if accessed directly
